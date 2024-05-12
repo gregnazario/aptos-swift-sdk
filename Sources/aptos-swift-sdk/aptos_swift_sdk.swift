9e2107adfdfa721847317a11a8c829c478f180d1
Greg Nazario <greg@gnazar.io>
2024-05-11 20:57:02 -0700

    initial attempt at bcs

--- Sources/aptos-swift-sdk/aptos_swift_sdk.swift
+++ Sources/aptos-swift-sdk/aptos_swift_sdk.swift
@@ -1,2 +1,101 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+
+
+// TODO: U128 U256
+public class Serializer {
+    var _data: [UInt8]
+    
+    public init() {
+        self._data = []
+    }
+    
+    public func consume() -> [UInt8] {
+        let data = self._data
+        self._data = []
+        return data
+    }
+    
+    func u8(input: UInt8) {
+        self._data.append(input)
+    }
+    
+    func u16(input: UInt16) {
+        self._data.append(UInt8(input & 0xFF))
+        self._data.append(UInt8((input >> 8) & 0xFF))
+    }
+    
+    func u32(input: UInt32) {
+        self._data.append(UInt8(input & 0xFF))
+        self._data.append(UInt8((input >> 8) & 0xFF))
+        self._data.append(UInt8((input >> 16) & 0xFF))
+        self._data.append(UInt8((input >> 24) & 0xFF))
+    }
+    
+    func u64(input: UInt64) {
+        self._data.append(UInt8(input & 0xFF))
+        self._data.append(UInt8((input >> 8) & 0xFF))
+        self._data.append(UInt8((input >> 16) & 0xFF))
+        self._data.append(UInt8((input >> 24) & 0xFF))
+        self._data.append(UInt8((input >> 32) & 0xFF))
+        self._data.append(UInt8((input >> 40) & 0xFF))
+        self._data.append(UInt8((input >> 48) & 0xFF))
+        self._data.append(UInt8((input >> 56) & 0xFF))
+    }
+    
+    func toBytes() -> [UInt8] {
+        return self._data
+    }
+}
+
+// TODO: U128 U256
+public class Deserializer {
+    var _data: [UInt8]
+    var _cursor: Int
+    
+    public init(data: [UInt8]) {
+        self._data = data
+        self._cursor = 0
+    }
+    
+    func u8() -> UInt8 {
+        let ret = self._data[self._cursor]
+        _cursor += 1
+        return ret
+    }
+    
+    func u16() -> UInt16{
+        var ret = UInt16(self.u8())
+        print(ret)
+        ret += UInt16(self.u8()) << 8
+        print(ret)
+        return ret
+    }
+    
+    func u32() -> UInt32 {
+        var ret = UInt32(self.u8())
+        ret += UInt32(self.u8()) << 8
+        ret += UInt32(self.u8()) << 16
+        ret += UInt32(self.u8()) << 24
+       
+        return ret
+    }
+    
+    func u64() -> UInt64 {
+        var ret = UInt64(self.u8())
+        ret += UInt64(self.u8()) << 8
+        ret += UInt64(self.u8()) << 16
+        ret += UInt64(self.u8()) << 24
+        ret += UInt64(self.u8()) << 32
+        ret += UInt64(self.u8()) << 40
+        ret += UInt64(self.u8()) << 48
+        ret += UInt64(self.u8()) << 56
+       
+        return ret
+    }
+    
+    func toBytes() -> [UInt8] {
+        return self._data
+    }
+}
--- Tests/aptos-swift-sdkTests/aptos_swift_sdkTests.swift
+++ Tests/aptos-swift-sdkTests/aptos_swift_sdkTests.swift
@@ -2,11 +2,75 @@ import XCTest
 @testable import aptos_swift_sdk
 
 final class aptos_swift_sdkTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testU8() throws {
+        let inputs: [UInt8] = [0,1,255]
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let serializer = Serializer()
+        for input in inputs {
+            serializer.u8(input: input)
+        }
+
+        assert(inputs == serializer.toBytes())
+        
+        let deserializer = Deserializer(data: inputs)
+        
+        for input in inputs {
+            assert(input == deserializer.u8())
+        }
+    }
+    
+    func testU16() throws {
+        let inputs: [UInt16] = [0, 1, 255, 65535]
+        let serialized: [UInt8] = [0, 0, 1, 0, 255, 0, 255, 255]
+
+        let serializer = Serializer()
+        for input in inputs {
+            serializer.u16(input: input)
+        }
+
+        assert(serialized == serializer.toBytes())
+        
+        let deserializer = Deserializer(data: serialized)
+        
+        for input in inputs {
+            let output = deserializer.u16()
+            assert(input == output)
+        }
+    }
+    
+    func testU32() throws {
+        let inputs: [UInt32] = [0, 1, 255, 65535]
+        let serialized: [UInt8] = [0, 0, 0, 0, 1, 0, 0, 0, 255, 0, 0, 0, 255, 255, 0, 0]
+
+        let serializer = Serializer()
+        for input in inputs {
+            serializer.u32(input: input)
+        }
+
+        assert(serialized == serializer.toBytes())
+        
+        let deserializer = Deserializer(data: serialized)
+        
+        for input in inputs {
+            assert(input == deserializer.u32())
+        }
+    }
+    
+    func testU64() throws {
+        let inputs: [UInt64] = [0, 1, 255, 65535]
+        let serialized: [UInt8] = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0, 0, 0, 0, 0, 0, 255, 255, 0, 0, 0, 0, 0, 0,]
+
+        let serializer = Serializer()
+        for input in inputs {
+            serializer.u64(input: input)
+        }
+
+        assert(serialized == serializer.toBytes())
+        
+        let deserializer = Deserializer(data: serialized)
+        
+        for input in inputs {
+            assert(input == deserializer.u64())
+        }
     }
 }
